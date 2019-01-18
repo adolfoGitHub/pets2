86d90e7bed7ae403b691231d231d6b858a6b29b5
adolfo <agonzalez15@mail.greenriver.edu>
2019-01-18 13:13:12 -0500

    added initial files

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload
+require_once('vendor/autoload.php');
+
+//create an instance of the BASE CLASS
+$f3 = Base::instance();
+
+//turn on fat-free error reporting
+$f3->set('DEBUG', 3);
+
+//define a default route
+$f3->route('GET /', function(){
+
+   //echo "<h1>Howdy!</h1>";
+
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//run fat free framework
+$f3->run();
